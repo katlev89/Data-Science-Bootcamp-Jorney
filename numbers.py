63785f26b00f87331227bc9523879c92c3c43ec6
Kateryna Levchenko <katja.levchenko@googlemail.com>
2024-03-07 00:32:25 +0000

    Updated variable names to clarify the code

--- numbers.py
+++ numbers.py
@@ -1,19 +1,19 @@
 # Ask the user to enter three different integers
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
-num3 = int(input("Enter the third number: "))
+number_1 = int(input("Enter the first number: "))
+number_2 = int(input("Enter the second number: "))
+number_3 = int(input("Enter the third number: "))
 
 # Calculate the sum of the numbers in the list
-sum_of_numbers = num1 + num2 + num3
+sum_of_numbers = number_1 + number_2 + number_3
 
 # Print the result
 print("The sum of the three numbers is:", sum_of_numbers)
 
 # Substraction of the second number from the first number:
-print("The first number minus the second number is: ", num1 - num2)
+print("The first number minus the second number is: ", number_1 - number_2)
 
 # Multiplication of the third number by the first number:
-print("The third number multiplied by the first number is: ", num3 * num1)
+print("The third number multiplied by the first number is: ", number_3 * number_1)
 
 # Division of the sum of three numbers by the third number:
-print("The sum devided by the third number is: ", sum_of_numbers / num3)
+print("The sum devided by the third number is: ", sum_of_numbers / number_3)
